06815c683ce3c933237f8d97118826c67a7b4e75
m4tsu <matsurec0147@gmail.com>
2018-08-13 13:51:12 +0000

    Add posts model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get '/' => "home#top"
+  get "about" => "home#about"
+  
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id" => "posts#show"
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
